6743ee5a4903cad5b8cd74af646d4f54068549ef
Eric Levine <djlevine@gmail.com>
2014-01-06 17:11:18 -0800

    added gumball project

---
+++ javascripts/gumball.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+	setInterval(handleRefresh, 3000);
+}
+
+function handleRefresh() {
+	var url = "http://gumball.wickedlysmart.com/?callback=updateSales" + 
+				"&random=" + (new Date()).getTime();
+	var newScriptElement = document.createElement("script");
+	newScriptElement.setAttribute("src", url);
+	newScriptElement.setAttribute("id", "jsonp");
+	
+	//if this exists, we'll just replace it
+	var oldScriptElement = document.getElementById("jsonp");
+	var head = document.getElementsByTagName("head")[0];
+
+	if (oldScriptElement == null) {
+		head.appendChild(newScriptElement);
+	} else {
+		head.replaceChild(newScriptElement, oldScriptElement);
+	}
+}
+
+function updateSales(sales) {
+	var salesDiv = document.getElementById("sales");
+	for (var i = 0; i < sales.length; i++) {
+		var sale = sales[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "saleItem");
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+		salesDiv.appendChild(div);
+	}
+}
+
+function refreshSales() {
+	var salesDiv = document.getElementById("sales");
+	salesDiv.appendElement()
+	
+}
