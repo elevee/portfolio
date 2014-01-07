1950e2baa8013fcb576188370735bf94e83da409
Eric Levine <djlevine@gmail.com>
2014-01-06 20:44:32 -0800

    testing browser compatibility

--- javascripts/tweetshirt.js
+++ javascripts/tweetshirt.js
@@ -1,9 +1,15 @@
 window.onload = function() {
+
 	var canvas = document.getElementById("tshirtCanvas");
 	
-	//need a 2D context to draw something
-	var context = canvas.getContext("2d");
-
-	context.fillRect(10, 10, 100, 100);
+	if (canvas.getContext) {
+		//browser has canvas support
+		//need a 2D context to draw something
+		var context = canvas.getContext("2d");
+		context.fillRect(10, 10, 100, 100);
+	} else {
+		canvas.innerHTML("<p>You straight-up don't have canvas support. Please update your browser! :)</p>");
+	}
+		
 
 }
